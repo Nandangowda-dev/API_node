33c667e0170666197db641a706166283fe640cea
Nandan Gowda <151739770+Nandangowda-dev@users.noreply.github.com>
2025-02-27 15:14:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,29 +25,29 @@ app.all("*",(req,res,next)=>{
 app.use(globalError);
 
 
-const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
-const express=require('express')
-const morgan=require('morgan')
-const userRoute=require('./Router/userRoute');
-const authRoute=require('./Router/AuthRoute');
-const customError = require('./utils/ErrorHandler');
-const globalError = require('./Controller/ErrroController');
-let app=express();
+// const dotenv = require('dotenv');
+// dotenv.config({path:'./config.env'});
+// const express=require('express')
+// const morgan=require('morgan')
+// const userRoute=require('./Router/userRoute');
+// const authRoute=require('./Router/AuthRoute');
+// const customError = require('./utils/ErrorHandler');
+// const globalError = require('./Controller/ErrroController');
+// let app=express();
 
-app.use(express.json());
-app.use(require('express-pdf'));
-if(process.env.NODE_ENV === "development"){
-    app.use(morgan('dev'));
-}
+// app.use(express.json());
+// app.use(require('express-pdf'));
+// if(process.env.NODE_ENV === "development"){
+//     app.use(morgan('dev'));
+// }
 
-app.use('/api/v1/user',userRoute);
-app.use('/api/v1/User',authRoute);
+// app.use('/api/v1/user',userRoute);
+// app.use('/api/v1/User',authRoute);
 
-app.all("*",(req,res,next)=>{
-    const error= new customError("Not Found",501);
-    next(error);
-})
-app.use(globalError);
+// app.all("*",(req,res,next)=>{
+//     const error= new customError("Not Found",501);
+//     next(error);
+// })
+// app.use(globalError);
 
 module.exports=app;
