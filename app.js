d50d22c2bd6f7b4c61e1bcb08908826518311b7c
Nandan Gowda <151739770+Nandangowda-dev@users.noreply.github.com>
2025-02-28 14:41:44 +0530

    node api changes

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
-<<<<<<< HEAD
 const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
-const express=require('express')
+const express=require('express');
+const cors = require('cors');
 const morgan=require('morgan')
 const userRoute=require('./Router/userRoute');
 const authRoute=require('./Router/AuthRoute');
 const customError = require('./utils/ErrorHandler');
 const globalError = require('./Controller/ErrroController');
 let app=express();
-
+app.use(cors({
+    origin: 'http://localhost:3000',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 app.use(express.json());
 app.use(require('express-pdf'));
 if(process.env.NODE_ENV === "development"){
     app.use(morgan('dev'));
 }
+
 
 app.use('/api/v1/user',userRoute);
 app.use('/api/v1/User',authRoute);
@@ -24,31 +29,4 @@ app.all("*",(req,res,next)=>{
 })
 app.use(globalError);
 
-=======
-const dotenv = require('dotenv');
-dotenv.config({path:'./config.env'});
-const express=require('express')
-const morgan=require('morgan')
-const userRoute=require('./Router/userRoute');
-const authRoute=require('./Router/AuthRoute');
-const customError = require('./utils/ErrorHandler');
-const globalError = require('./Controller/ErrroController');
-let app=express();
-
-app.use(express.json());
-app.use(require('express-pdf'));
-if(process.env.NODE_ENV === "development"){
-    app.use(morgan('dev'));
-}
-
-app.use('/api/v1/user',userRoute);
-app.use('/api/v1/User',authRoute);
-
-app.all("*",(req,res,next)=>{
-    const error= new customError("Not Found",501);
-    next(error);
-})
-app.use(globalError);
-
->>>>>>> 071a5bf827fa85798a925b0a325656593f91e8e9
 module.exports=app;
--- server.js
+++ server.js
@@ -1,10 +1,14 @@
+const cors = require('cors');
 const mongoose=require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({path:'./config.env'});
 const app=require('./app');
 
-
-
+app.use(cors({
+    origin: ['http://localhost:3000','https://api-node-ilsv.onrender.com'],  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 mongoose.connect(process.env.LOCAL_DB)
     .then((connect)=>{ console.log('DB connected Succesfully')})
     .catch((error)=> {  console.log(error)     })
